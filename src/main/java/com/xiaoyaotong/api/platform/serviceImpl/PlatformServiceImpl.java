6a4a46520436a8a08f0fbad9433ef5573220ffd8
hejiabing <husthejiabing@126.com>
2019-12-01 20:16:02 +0800

    实现platformservice方法

--- src/main/java/com/xiaoyaotong/api/platform/serviceImpl/PlatformServiceImpl.java
+++ src/main/java/com/xiaoyaotong/api/platform/serviceImpl/PlatformServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 /**
  * @author ：billHe
- * @description：TODO
+ * @description：platformserice的方法实现
  * @date ：2019/12/1 7:40 PM
  */
 public class PlatformServiceImpl implements PlatformSkuService {
