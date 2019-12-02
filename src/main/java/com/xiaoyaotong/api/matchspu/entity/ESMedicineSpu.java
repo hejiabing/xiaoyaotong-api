d85915560b3cab8164827b367b29e71bcac0528f
hejiabing <husthejiabing@126.com>
2019-12-02 21:42:39 +0800

    修改标品的es entity

--- src/main/java/com/xiaoyaotong/api/matchspu/entity/ESMedicineSpu.java
+++ src/main/java/com/xiaoyaotong/api/matchspu/entity/ESMedicineSpu.java
@@ -13,25 +13,88 @@ import java.io.Serializable;
  * @date ：2019/12/2 7:26 PM
  */
 @Data
-@Document(indexName = "xiaoyaotong",type="standardspu")
+@Document(indexName = "xiaoyaotong",type="spu")
 public class ESMedicineSpu implements Serializable {
 
-    public String getCommonName() {
-        return commonName;
-    }
     public void setCommonName(String commonName) {
         this.commonName = commonName;
     }
 
-    @Field
-    private String commonName; //通用名
+    public void setSpuCode(String spuCode) {
+        this.spuCode = spuCode;
+    }
+
+    public void setApprovalCode(String approvalCode) {
+        this.approvalCode = approvalCode;
+    }
+
+    public void setSpec(String spec) {
+        this.spec = spec;
+    }
+
+    public void setFactoryName(String factoryName) {
+        this.factoryName = factoryName;
+    }
+
+    public void setFormalName(String formalName) {
+        this.formalName = formalName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    @Id
-    private int id;
+    public String getCommonName() {
+        return commonName;
+    }
 
+    public String getSpuCode() {
+        return spuCode;
+    }
+
+    public String getApprovalCode() {
+        return approvalCode;
+    }
+
+    public String getSpec() {
+        return spec;
+    }
+
+    public String getFactoryName() {
+        return factoryName;
+    }
+
+    public String getFormalName() {
+        return formalName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Field
+    private String commonName; //通用名
+    @Field
+    private String spuCode;// spu code
+    @Field
+    private String approvalCode; //批准文号
+    @Field
+    private String spec; //规格
+    @Field
+    private String factoryName; //生产厂家
+    @Field
+    private String formalName; //商品名
+    @Field
+    private String shortName; //别名
+    @Id
+    private int id;//id
 
 }
