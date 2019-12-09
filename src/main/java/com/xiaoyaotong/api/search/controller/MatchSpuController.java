2f8cefe2dea9987919748b170a84294662aeb600
hejiabing <husthejiabing@126.com>
2019-12-09 10:56:33 +0800

    修改标品搜索入口

--- src/main/java/com/xiaoyaotong/api/search/controller/MatchSpuController.java
+++ src/main/java/com/xiaoyaotong/api/search/controller/MatchSpuController.java
@@ -3,7 +3,6 @@ package com.xiaoyaotong.api.search.controller;
 import com.xiaoyaotong.api.search.entity.ESMedicineSpu;
 import com.xiaoyaotong.api.search.service.ESMedicineSpuService;
 import com.xiaoyaotong.api.search.service.EsSpuSearchService;
-import com.xiaoyaotong.api.standardproduct.entity.MedicineSPU;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +16,7 @@ import java.util.List;
  * @date ：2019/12/2 9:10 PM
  */
 @RestController
-@RequestMapping("/search/match")
+@RequestMapping("/search")
 public class MatchSpuController {
     @Autowired
     private ESMedicineSpuService eSMedicineSpuService;
@@ -29,14 +28,14 @@ public class MatchSpuController {
      * 通过controller向搜索里面批量同步标品库的数据
      * 目前供测试使用
      */
-    @RequestMapping("/addSpuList")
+    //@RequestMapping("/addSpuList")
     public void addSpu(List<ESMedicineSpu> spuList){
         for(ESMedicineSpu esMedicineSpu: spuList){
             eSMedicineSpuService.addMedicineSpu(esMedicineSpu);
         }
     }
 
-    @RequestMapping("/testsearch")
+    @RequestMapping("/spu")
     public List<ESMedicineSpu> testSearch(@RequestBody ESMedicineSpu esMedicineSpu){
         List<ESMedicineSpu> spus = esSpuSearchService.searchSpuList(esMedicineSpu);
         return spus;
