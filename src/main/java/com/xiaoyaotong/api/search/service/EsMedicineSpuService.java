9073efce3057ae4b2a0a0bc968c730fb14352798
hejiabing <husthejiabing@126.com>
2019-12-15 17:22:10 +0800

    修改文件名字

--- src/main/java/com/xiaoyaotong/api/search/service/EsMedicineSpuService.java
+++ src/main/java/com/xiaoyaotong/api/search/service/EsMedicineSpuService.java
@@ -3,6 +3,7 @@ package com.xiaoyaotong.api.search.service;
 import com.xiaoyaotong.api.search.entity.EsMedicineSpu;
 
 public interface EsMedicineSpuService {
+    //push
     public void addMedicineSpu(EsMedicineSpu medicineSpu);
 
 }
